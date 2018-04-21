99c5cdbccf0366dc557605423ec2beb58f568890
robainaricardogit config --global user.name robainaricardo <ricardorobaina11@gmail.com>
2018-04-21 20:34:05 -0300

    Servidor conectando :D

--- ServerFTP.c
+++ ServerFTP.c
@@ -5,21 +5,37 @@
 #include<netdb.h>
 
 int main(){
-    
+
     int servidorSocket;
     int clienteSocket;
-    
-    struct sockaddr_in servidor, cliente;    
+
+    struct sockaddr_in servidor, cliente;
 
 
     //Cria um socket com (,Servico com conexão TCP,)
     servidorSocket = socket(AF_INET, SOCK_STREAM, 0);
-    
+    printf("Criou o socket com número %d\n", servidorSocket);
+    //se socket retornar -1 é que não conseguiu
 
-    //bind(servidorSocket, (struct sockaddr*)&servidor, sezeof(servidor));
+    //bzero(&servidor, sizeof(servidor)); //limpa os bytes
+    servidor.sin_family = AF_INET;  //protocolo internet
+    servidor.sin_addr.s_addr = htonl(INADDR_ANY); //qualquer interface de rede
+    servidor.sin_port = htons(2121); //porta
 
-    //listen(servidorSocket, 1);
 
-    printf("A biblioteca exite!\n");
+
+    bind(servidorSocket, (struct sockaddr*)&servidor, sizeof(servidor));
+    printf("Bind OK\n");
+
+
+    //listem pra uma conexão
+    listen(servidorSocket, 1);
+    printf("Listen OK!\n");
+
+    while(1){
+      int clienteSocket = accept(servidorSocket, (struct  sockaddr *) NULL, NULL);
+    }
+
+
     return 0;
 }
